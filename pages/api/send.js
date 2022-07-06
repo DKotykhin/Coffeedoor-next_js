74ddec0eae6e5931ead2d150523a5b0410a45119
Dmytro Kotykhin <102535598+DKotykhin@users.noreply.github.com>
2022-07-06 22:02:38 +0200

    Update send.js

--- pages/api/send.js
+++ pages/api/send.js
@@ -2,41 +2,43 @@ import axios from "axios";
 // import dotenv from 'dotenv';
 // dotenv.config();
 
-export default function handler(req, res) {
-    const TOKEN = process.env.TELEGRAM_TOKEN;
-    const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-    if (!TOKEN) {
-        throw new Error("Define TOKEN environmental variable");
-    }
-    if (!CHAT_ID) {
-        throw new Error("Define CHAT_ID environmental variable");
-    }
-    const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
-    let message = `<b>Заявка з сайту</b>\n`;
-    let itemSum = 0;
-    req.body.forEach((item) => {
-        if ('phone' in item) {
-            message += `<b>Відправник: </b>${item.name}\n`;
-            message += `<b>Телефон: </b>${item.phone}\n`;
-            message += `<b>Спосіб доставки: </b>${item.delivery}\n`;
-            message += `<b>Коментар: </b>${item.text ? item.text : ''}\n`;
-            message += `<b>Заказ: </b>\n`;
+export default async function handler(req, res) {
+    try {
+        const TOKEN = process.env.TELEGRAM_TOKEN;
+        const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+        if (!TOKEN) {
+            throw new Error("Define TOKEN environmental variable");
         }
-        if ('title' in item) {
-            itemSum += +item.price;
-            message += `${item.title} ${item.name} ${item.quantity} x ${item.price} грн\n`;
+        if (!CHAT_ID) {
+            throw new Error("Define CHAT_ID environmental variable");
         }
-    });
-    message += `<b>Всього на сумму: </b>${itemSum} грн`;
+        const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+        let message = `<b>Заявка з сайту</b>\n`;
+        let itemSum = 0;
+        req.body.forEach((item) => {
+            if ('phone' in item) {
+                message += `<b>Відправник: </b>${item.name}\n`;
+                message += `<b>Телефон: </b>${item.phone}\n`;
+                message += `<b>Спосіб доставки: </b>${item.delivery}\n`;
+                message += `<b>Коментар: </b>${item.text ? item.text : ''}\n`;
+                message += `<b>Заказ: </b>\n`;
+            }
+            if ('title' in item) {
+                itemSum += +item.price;
+                message += `${item.title} ${item.name} ${item.quantity} x ${item.price} грн\n`;
+            }
+        });
+        message += `<b>Всього на сумму: </b>${itemSum} грн`;
 
-    axios.post(URL, {
-        chat_id: CHAT_ID,
-        parse_mode: "html",
-        text: message,
-    }).catch(error => {
-        console.log(error.message);
-    });
+        const resp = await axios.post(URL, {
+            chat_id: CHAT_ID,
+            parse_mode: "html",
+            text: message,
+        })
 
-    res.status(200).json({ name: req.body })
-
+        return res.status(200).json({ name: req.body, resp })
+    } catch(err) {
+        console.log('err', err)
+        return res.status(500).json({ err })
+    }
 }
